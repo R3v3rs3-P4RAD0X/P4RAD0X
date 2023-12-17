405d64e630d3a84d4b013e1bb3b980090190a222
StrangeParadox <d.ellis01.dje@gmail.com>
2023-12-17 09:55:42 +0000

    Remove unused files and configurations
    Created a project creator script.

---
+++ projects/python/archer/README.md
@@ -0,0 +1,3 @@
+# Archer
+
+A set of scripts used during post-install to setup Arch Linux distros.
--- projects/setup-arch/src/components/__init__.py
+++
@@ -1,4 +0,0 @@
-from components.pacman import Pacman
-from components.print import Printer
-from components.sshd import SSHD
-from components.sudoers import Sudoers
